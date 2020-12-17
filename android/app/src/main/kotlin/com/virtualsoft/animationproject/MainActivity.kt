563f7a8cec71b762e0c5c762cee3a2e88a032cd4
Mian Umar Arshad <mianumar@Mians-MacBook-Pro.local>
2020-12-17 10:24:39 +0500

    adding project
    adding my project in git

---
+++ android/app/src/main/kotlin/com/virtualsoft/animationproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.virtualsoft.animationproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({this.title});
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Animation <double>animation;
+  AnimationController animationController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController=AnimationController(duration: const Duration(seconds: 3),vsync: this);
+    animation=
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
